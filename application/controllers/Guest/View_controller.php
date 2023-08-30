55efbb011c6c396a0946d8c747e3c75d21e1eea2
Ziaul Kamal <codepad@usa.com>
2023-08-30 17:25:58 +0700

    mailing

---
+++ application/controllers/Guest/View_controller.php
@@ -0,0 +1,21 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class View_controller extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+
+    function form_permhonan() : Returntype {
+        
+    }
+}
+
+/* End of file View_controller.php and path \application\controllers\Guest\View_controller.php */
